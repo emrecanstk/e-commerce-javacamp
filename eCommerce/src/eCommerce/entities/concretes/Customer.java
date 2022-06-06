4dbd665709721f04f51aad3c89eb1f9cb65a2bff
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:21:07 +0300

    added constructure on Customer

--- eCommerce/src/eCommerce/entities/concretes/Customer.java
+++ eCommerce/src/eCommerce/entities/concretes/Customer.java
@@ -4,4 +4,19 @@ import eCommerce.entities.abstracts.Entity;
 
 public class Customer implements Entity {
 	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	public Customer(int id, String firstName, String lastName, String email, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	
 }
