dbce07fd94c7f8590d60dd59f9a566085836174a
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:53:06 +0300

    validator is filled

--- eCommerce/src/eCommerce/core/concretes/EmailValidator.java
+++ eCommerce/src/eCommerce/core/concretes/EmailValidator.java
@@ -1,5 +1,7 @@
 package eCommerce.core.concretes;
 
+import java.util.regex.Pattern;
+
 import eCommerce.core.abstracts.EmailService;
 import eCommerce.entities.concretes.Customer;
 
@@ -7,8 +9,22 @@ public class EmailValidator implements EmailService {
 
 	@Override
 	public boolean validator(Customer customer) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		if(customer.getEmail() == null || customer.getEmail().isEmpty()) {
+			return false;
+		}
+		
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+				            "[a-zA-Z0-9_+&*-]+)*@"+
+				            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+		
+		Pattern pattern = Pattern.compile(emailRegex);
+					
+		if(pattern.matcher(customer.getEmail()).matches()) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
-
 }
