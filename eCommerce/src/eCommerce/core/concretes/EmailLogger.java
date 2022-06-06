7a15e2c268c4095630a4fe72bedda11c1badf0f1
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:51:14 +0300

    verify method is filled

--- eCommerce/src/eCommerce/core/concretes/EmailLogger.java
+++ eCommerce/src/eCommerce/core/concretes/EmailLogger.java
@@ -1,12 +1,20 @@
 package eCommerce.core.concretes;
 
+import java.util.Scanner;
+
 import eCommerce.core.abstracts.LoggerService;
 
 public class EmailLogger implements LoggerService {
 
 	@Override
 	public void verify(String message) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Link has been sent to your e-mail adress.\n");
+		Scanner confirm = new Scanner(System.in);
+		
+		int input = confirm.nextInt();
+		if(input == 1) { System.out.println(message + " successful." + "\n"); }
+		else { System.out.println(message + " unsuccessful!" + "\n"); }
 		
 	}
 
