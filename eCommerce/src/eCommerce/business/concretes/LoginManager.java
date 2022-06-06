300b67f3308b984bfdb92a393c21425a32484fee
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:47:44 +0300

    login method is filled

--- eCommerce/src/eCommerce/business/concretes/LoginManager.java
+++ eCommerce/src/eCommerce/business/concretes/LoginManager.java
@@ -1,12 +1,27 @@
 package eCommerce.business.concretes;
 
 import eCommerce.business.abstracts.LoginService;
+import eCommerce.dataAccess.abstracts.CustomerDao;
+import eCommerce.entities.concretes.Customer;
 
 public class LoginManager implements LoginService {
-
+	
+	private CustomerDao customerDao;
+	
+	public LoginManager(CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+	
 	@Override
 	public void login(String email, String password) {
-		// TODO Auto-generated method stub
+		for(Customer customer : this.customerDao.getAll()) {
+			if(customer.getEmail() == email || customer.getPassword() == password) {
+				System.out.println(customer.getFirstName() + " is succesfuly logined.");
+			}
+			else {
+				System.out.println("Your email or password is invalid.");
+			}
+		}
 		
 	}
 
