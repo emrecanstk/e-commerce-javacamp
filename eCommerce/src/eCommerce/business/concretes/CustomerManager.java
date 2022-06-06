218c6e3532f234244e230e0eddfe45a19c203366
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:44:38 +0300

    CustomerManager's methods are created and filled

--- eCommerce/src/eCommerce/business/concretes/CustomerManager.java
+++ eCommerce/src/eCommerce/business/concretes/CustomerManager.java
@@ -3,19 +3,58 @@ package eCommerce.business.concretes;
 import java.util.List;
 
 import eCommerce.business.abstracts.CustomerService;
+import eCommerce.core.abstracts.EmailService;
+import eCommerce.core.abstracts.LoggerService;
+import eCommerce.core.abstracts.RegisterService;
+import eCommerce.dataAccess.abstracts.CustomerDao;
 import eCommerce.entities.concretes.Customer;
 
 public class CustomerManager implements CustomerService {
-
+	
+	private CustomerDao customerDao;
+	private EmailService emailService;
+	private LoggerService loggerService;
+	private RegisterService registerService;
+	
+	public CustomerManager(RegisterService registerService, LoggerService loggerService) {
+		this.registerService = registerService;
+		this.loggerService = loggerService;
+	}
+	
+	public CustomerManager(CustomerDao customerDao, EmailService emailService, LoggerService loggerService) {
+		this.customerDao = customerDao;
+		this.emailService = emailService;
+		this.loggerService = loggerService;
+	}
+	
 	@Override
 	public void memberAdd(Customer customer) {
-		// TODO Auto-generated method stub
+		
+		if(customer.getPassword().length() <= 5 || emailService.validator(customer) == false) {
+			System.out.println("Your email or password is invalid." + "\n");
+			return;
+		}
+		
+		for(Customer user:this.customerDao.getAll()) {
+			if(user.getEmail() == customer.getEmail()) {
+				System.out.println("This email already exists." + "\n");
+				return;
+			};
+		}
+		
+		if(customer.getFirstName().length() <= 1 || customer.getFirstName().length() <= 1) {
+			System.out.println("Your name or surname is invalid." + "\n");
+			return;
+		}
+		
+		this.loggerService.verify(customer.getFirstName());
+		this.customerDao.add(customer);
 		
 	}
 
 	@Override
 	public void memberDelete(Customer customer) {
-		// TODO Auto-generated method stub
+		customerDao.delete(customer);
 		
 	}
 
@@ -32,9 +71,9 @@ public class CustomerManager implements CustomerService {
 	}
 
 	@Override
-	public void memberAddWithGoogle(String email, String passport) {
-		// TODO Auto-generated method stub
-		
+	public void memberAddWithGoogle(String email, String password) {
+		this.loggerService.verify(email);
+		this.registerService.memberAdd(email, password);
 	}
 
 }
