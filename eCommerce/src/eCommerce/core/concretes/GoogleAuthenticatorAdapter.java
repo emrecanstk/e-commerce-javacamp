e794cd8b20bb19898b21c6d9fb453797edd7d15f
Emre Can Satık <emrecanstk@gmail.com>
2022-06-06 03:55:02 +0300

    register operations are set

--- eCommerce/src/eCommerce/core/concretes/GoogleAuthenticatorAdapter.java
+++ eCommerce/src/eCommerce/core/concretes/GoogleAuthenticatorAdapter.java
@@ -1,12 +1,14 @@
 package eCommerce.core.concretes;
 
 import eCommerce.core.abstracts.RegisterService;
+import eCommerce.googleApi.GoogleAuthenticator;
 
 public class GoogleAuthenticatorAdapter implements RegisterService {
 
 	@Override
 	public void memberAdd(String email, String password) {
-		// TODO Auto-generated method stub
+		GoogleAuthenticator member = new GoogleAuthenticator();
+		member.register(email, password);
 		
 	}
 
--- eCommerce/src/eCommerce/googleApi/GoogleAuthenticator.java
+++ eCommerce/src/eCommerce/googleApi/GoogleAuthenticator.java
@@ -1,5 +1,7 @@
 package eCommerce.googleApi;
 
 public class GoogleAuthenticator {
-
+	public void register(String email, String password) {
+		System.out.println("Registered with Google.");
+	}
 }
